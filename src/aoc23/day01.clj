3b1b4fc8c2ebb3a79d7cc5c80816efc66e519be0
bfollek <bfollek@gmail.com>
2023-12-01 16:05:24 -0500

    Work In Progress

--- src/aoc23/day01.clj
+++ src/aoc23/day01.clj
@@ -1,5 +1,6 @@
 (ns aoc23.day01
   (:require
+    ;[clojure.string as str]
    [clojure.java.io :as io]
    [rabbithole.core :as rh]))
 
@@ -11,6 +12,13 @@
     ;(println cal-digits rv)
     rv))
 
+(defn words-to-ints
+  [s]
+  (clojure.string/replace s
+                          #"one|two|three|four|five|six|seven|eight|nine"
+                          {"one" "1" "two" "2" "three" "3" "four" "4" "five" "5"
+                           "six" "6" "seven" "7" "eight" "8" "nine" "9"}))
+
 (defn part-1
   "On each line, the calibration value can be found by combining the first digit and the last digit (in that order) to form a single two-digit number."
   [file-name]
@@ -18,5 +26,11 @@
     (reduce + (map #(calibration-value %) (line-seq rdr)))))
 
 (defn part-2
-  "...some of the digits are actually spelled out with letters: one, two, three, four, five, six, seven, eight, and nine also count as valid 'digits'."
-  [file-name])
+  " From the spec: '...some of the digits are actually spelled out with letters: one, two, three, four, five, six, seven, eight, and nine also count as valid 'digits'.'
+   
+   There are ambiguous cases, e.g. is 'oneight' 'one' or 'eight'? What about 'twone'? etc.
+   "
+  [file-name]
+  (with-open [rdr (io/reader file-name)]
+    (reduce + (map #((comp calibration-value words-to-ints) %) (line-seq rdr)))))
+
--- test/aoc23/day01_test.clj
+++ test/aoc23/day01_test.clj
@@ -4,8 +4,8 @@
 
 (deftest day01-happy-path
   (testing "part-1."
-    (is (= 142 (part-1 "data/day01_short.txt")))
+    (is (= 142 (part-1 "data/day01_short_part_1.txt")))
     (is (= 54877 (part-1 "data/day01_long.txt")))))
-  ;; (testing "part-2."
-  ;;   (is (= 45000 (part-2 "data/day01_short.txt")))
-  ;;   (is (= 213159 (part-2 "data/day01_long.txt")))))
+(testing "part-2."
+  (is (= 281 (part-2 "data/day01_short_part_2.txt"))
+      (is (= 54095 (part-2 "data/day01_long.txt")))))
